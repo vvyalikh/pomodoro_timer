eb16593c7d911ab142cc1488039e44b5dc377a0a
vvyalikh <veronika.vyalikh@gmail.comgit config --global user.email veronika.vyalikh@gmail.comgit config user.email veronika.vyalikh@gmail.com>
2021-08-29 19:41:11 +0200

    final version of pomodoro timer

--- main.py
+++ main.py
@@ -9,34 +9,73 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-
+reps = 0
+timer = None
 # ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    title_label.config(text="Timer", fg=GREEN)
+    check_marks.config(text="")
+    #to reset reps and start from the very beginning
+    global reps
+    reps = 0
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
+
 def timer_button():
-    countdown(5*60)
+    global reps
+    reps += 1
+    work_sec = WORK_MIN*60
+    short_break_sec = SHORT_BREAK_MIN*60
+    long_break_sec = LONG_BREAK_MIN*60
+
+    if reps % 8 == 0:
+        countdown(long_break_sec)
+        title_label.config(text="Long Break", fg=RED)
+    elif reps % 2 == 0:
+        countdown(short_break_sec)
+        title_label.config(text="Short Break", fg=PINK)
+    else:
+        countdown(work_sec)
+        title_label.config(text="Time to work", fg=GREEN)
+
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def countdown(count):
     count_min = math.floor(count/60)
     count_sec = count % 60
+    #dynamic typing. Change of variable type from int to a string
+    if count_sec == 0:
+        count_sec = "00"
+    elif count_sec < 10:
+        count_sec = f"0{count_sec}"
 
     #to change exact item in canvas
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     print(count)
     if count > 0:
-        window.after(1000, countdown, count-1)
+        global timer
+        timer = window.after(1000, countdown, count-1)
+    else:
+        timer_button()
+        marks = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            marks += "✔"
+        check_marks.config(text=marks)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("p_timer")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-
-#grid shows where title is on grid
+# grid shows where title is on grid
 title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 36, "bold"))
 title_label.grid(column=1, row=0)
+
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
@@ -50,10 +89,10 @@ start_button = Button(text="Start", fg=PINK, bg=YELLOW, font=(FONT_NAME, 18, "bo
 start_button.grid(column=0, row=3)
 
 
-reset_button = Button(text="Reset", fg=PINK, bg=YELLOW, font=(FONT_NAME, 18, "bold"), highlightthickness=5)
+reset_button = Button(text="Reset", fg=PINK, bg=YELLOW, font=(FONT_NAME, 18, "bold"), highlightthickness=5, command=reset_timer)
 reset_button.grid(column=3, row=3)
 
-check_marks = Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 20, "italic"))
+check_marks = Label(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 20, "italic"))
 check_marks.grid(column=1, row=3)
 
 
